81ec18d7e24530d0bd96d342c383ee09b00a4521
Robert Westerlund <robert.westerlund@live.com>
2022-12-11 02:50:52 +0100

    Added 2022 days 05, 06 and 07 in F#

---
+++ 2022/05/Program.fs
@@ -0,0 +1,83 @@
+﻿type Instruction =
+    { Count: int32
+      Source: int32
+      Destination: int32 }
+
+let parse (input: string) =
+    let [| initState; instructions |] =
+        input.Replace("\r\n", "\n").Split("\n\n")
+
+    let initStateLines = initState.Split("\n")
+
+    let stackCountLine =
+        initStateLines.[initStateLines.Length - 1]
+
+    let numberOfStacks =
+        System.Int32.Parse(
+            stackCountLine.[stackCountLine.Length - 1]
+                .ToString()
+        )
+
+    (initStateLines
+     |> Array.fold
+         (fun acc curr ->
+             match curr.Trim().[0] with
+             | '[' ->
+                 acc
+                 |> List.mapi (fun index stack ->
+                     if curr.Length > index * 4 then
+                         match curr.[(index * 4) + 1] with
+                         | ' ' -> stack
+                         | x -> stack @ [ x ]
+                     else
+                         stack)
+             | _ -> acc)
+         ([ 1 .. numberOfStacks ] |> List.map (fun _ -> [])),
+     (instructions.Split("\n")
+      |> Array.toList
+      |> List.map (fun (inst: string) ->
+          let split = inst.Split(" ")
+
+          { Count = System.Int32.Parse(split.[1])
+            Source = System.Int32.Parse(split.[3])
+            Destination = System.Int32.Parse(split.[5]) })))
+
+let move groupGrabConverter input =
+    let (state, instructions) = input |> parse
+
+    instructions
+    |> List.fold
+        (fun acc curr ->
+            acc
+            |> List.mapi (fun index stack ->
+                match index + 1 with
+                | x when x = curr.Source -> stack |> List.skip curr.Count
+                | x when x = curr.Destination ->
+                    List.append
+                        (acc.[curr.Source - 1]
+                         |> List.take curr.Count
+                         |> groupGrabConverter)
+                        stack
+                | _ -> stack))
+        state
+    |> List.fold (fun acc stack -> acc @ [ stack |> List.head ]) []
+    |> List.fold (fun acc curr -> acc + curr.ToString()) ""
+
+let part1 = move List.rev
+let part2 = move (fun s -> s)
+
+Input.testData
+|> part1
+|> printfn "Part 1 - test data: %A"
+
+Input.realData
+|> part1
+|> printfn "Part 1 - real data: %A"
+
+Input.testData
+|> part2
+|> printfn "Part 2 - test data: %A"
+
+Input.realData
+|> part2
+|> printfn "Part 2 - real data: %A"
---
+++ 2022/07/Program.fs
@@ -0,0 +1,143 @@
+﻿type CommandType =
+    | LS
+    | CD of target: string
+
+type File = { Name: string; Size: int64 }
+
+type Line =
+    | Command of command: CommandType
+    | Directory of name: string
+    | FileEntry of File
+
+type Directory =
+    { Name: string
+      Parent: Directory option
+      mutable Directories: Directory list
+      mutable Files: File list }
+
+    member this.getDirectorySize() =
+        (this.Directories
+         |> List.sumBy (fun d -> d.getDirectorySize ()))
+        + (this.Files |> List.sumBy (fun f -> f.Size))
+
+type Context =
+    { Root: Directory
+      mutable Cwd: Directory }
+
+let parse (input: string) =
+    input.Replace("\r\n", "\n").Split("\n")
+    |> Array.toList
+    |> List.map (fun text ->
+        if text.[0] = '$' then
+            let splitCommand = text.Split(" ")
+
+            match splitCommand.[1] with
+            | "cd" -> Command(CD splitCommand.[2])
+            | "ls" -> Command(LS)
+        elif text.StartsWith("dir") then
+            Directory(text.Split(" ").[1])
+        else
+            let split = text.Split(" ")
+
+            FileEntry
+                { Name = split.[1]
+                  Size = System.Int64.Parse(split.[0]) })
+
+let createTree input =
+    let root =
+        { Name = "/"
+          Parent = None
+          Directories = []
+          Files = [] }
+
+    let rec interpret (context: Context) lines =
+        match lines with
+        | [] -> context
+        | h :: t ->
+            match h with
+            | FileEntry file ->
+                context.Cwd.Files <- context.Cwd.Files |> List.append [ file ]
+                t |> interpret context
+            | Directory dir ->
+                context.Cwd.Directories <-
+                    context.Cwd.Directories
+                    |> List.append [ { Name = dir
+                                       Parent = Some context.Cwd
+                                       Directories = []
+                                       Files = [] } ]
+
+                t |> interpret context
+            | Command command ->
+                match command with
+                | LS -> t |> interpret context
+                | CD path ->
+                    match path with
+                    | "/" -> t |> interpret { context with Cwd = context.Root }
+                    | ".." ->
+                        t
+                        |> interpret { context with Cwd = context.Cwd.Parent |> Option.get }
+                    | x ->
+                        t
+                        |> interpret
+                            { context with
+                                Cwd =
+                                    context.Cwd.Directories
+                                    |> List.find (fun d -> d.Name = x) }
+
+    input
+    |> parse
+    |> interpret { Root = root; Cwd = root }
+
+
+let rec findDirs predicate cwd =
+    let matchingChildren =
+        cwd.Directories
+        |> List.fold (fun acc curr -> acc |> List.append (findDirs predicate curr)) []
+
+    if predicate cwd then
+        cwd :: matchingChildren
+    else
+        matchingChildren
+
+let part1 input =
+    let tree = input |> createTree
+
+    let mutableList =
+        new System.Collections.Generic.List<Directory>()
+
+    let dirs =
+        findDirs (fun (d: Directory) -> d.getDirectorySize () <= 100000L) tree.Root
+
+    dirs
+    |> List.sumBy (fun d -> d.getDirectorySize ())
+
+let part2 input =
+    let tree = input |> createTree
+    let totalSpace = 70000000L
+    let usedSpace = tree.Root.getDirectorySize ()
+    let availableSpace = totalSpace - usedSpace
+    let neededSpace = 30000000L
+    let missingSpace = -(availableSpace - neededSpace)
+
+    let candidates =
+        findDirs (fun d -> d.getDirectorySize () > missingSpace) tree.Root
+
+    candidates
+    |> List.map (fun d -> d.getDirectorySize ())
+    |> List.min
+
+Input.testData
+|> part1
+|> printfn "Part 1 - test data: %i"
+
+Input.realData
+|> part1
+|> printfn "Part 1 - real data: %i"
+
+Input.testData
+|> part2
+|> printfn "Part 2 - test data: %i"
+
+Input.realData
+|> part2
+|> printfn "Part 2 - real data: %i"
